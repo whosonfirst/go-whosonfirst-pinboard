b1e584e1b6fa283fb5b4f0df6097cf6e9f61dc71
thisisaaroland <aaron@mapzen.com>
2017-05-31 08:30:14 -0700

    fix per issue #1; start prepping to handle issue #2

--- cmd/wof-archive-url.go
+++ cmd/wof-archive-url.go
@@ -16,266 +16,16 @@ import (
 	"strings"
 )
 
-type Bookmark struct {
-	URL   string
-	Title string
-	Tags  []string
+type WaybackAPI struct {
 }
 
-func (bm Bookmark) ToParams() *url.Values {
+func NewWaybackAPI() (*WaybackAPI, error) {
 
-	params := url.Values{}
-
-	params.Set("url", bm.URL)
-	params.Set("description", bm.Title)
-	params.Set("tags", strings.Join(bm.Tags, " "))
-
-	return &params
+	wb := WaybackAPI{}
+	return &wb, nil
 }
 
-func main() {
-
-	var wofid = flag.Int64("wofid", 0, "A valid Who's On First ID")
-	var to_archive = flag.String("url", "", "The URL you want to bookmark.")
-	var token = flag.String("token", "", "A valid Pinboard API auth token.")
-	var data_root = flag.String("data-root", "https://whosonfirst.mapzen.com/data/", "...")
-
-	flag.Parse()
-
-	if *wofid == 0 {
-		log.Fatal("Missing WOF ID")
-	}
-
-	if *to_archive == "" {
-		log.Fatal("Missing URL")
-	}
-
-	if *token == "" {
-		log.Fatal("Missing Pinboard API token")
-	}
-
-	//
-
-	parsed, err := url.Parse(*to_archive)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	host := parsed.Host
-
-	if strings.HasPrefix(host, "www.") {
-		host = strings.Replace(host, "www.", "", -1)
-	}
-
-	// fetch the WOF record and extract hierarchy and concordances
-	// please move me in to a function or something...
-	// (20170530/thisisaaronland)
-
-	abs_url, err := uri.Id2AbsPath(*data_root, *wofid)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	rsp, err := http.Get(abs_url)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer rsp.Body.Close()
-
-	feature, err := ioutil.ReadAll(rsp.Body)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	hierarchy := make(map[string]string)
-
-	h := gjson.GetBytes(feature, "properties.wof:hierarchy")
-
-	h.ForEach(func(key, value gjson.Result) bool {
-
-		for placetype, id := range value.Map() {
-			hierarchy[id.String()] = placetype
-		}
-
-		return true // keep iterating
-	})
-
-	p := gjson.GetBytes(feature, "properties.wof:placetype")
-	placetype := p.String()
-
-	concordances := make([]string, 0)
-
-	c := gjson.GetBytes(feature, "properties.wof:concordances")
-
-	if c.Exists() {
-
-		c.ForEach(func(key, value gjson.Result) bool {
-
-			if key.String() != "sg:id" {
-				tag := fmt.Sprintf("%s=%s", key, value)
-				concordances = append(concordances, tag)
-			}
-
-			return true
-		})
-	}
-
-	wof_tags := make([]string, 0)
-
-	t := gjson.GetBytes(feature, "properties.wof:tags")
-
-	if t.Exists() {
-
-		t.ForEach(func(key, value gjson.Result) bool {
-			wof_tags = append(wof_tags, value.String())
-			return true
-		})
-	}
-
-	// get URL title
-
-	title, err := ParseURL(*to_archive)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// archive URL and get datetime stamp
-
-	dt, err := ArchiveURL(*to_archive)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// build bookmark
-
-	tags := []string{
-		host,
-		fmt.Sprintf("wof:placetype=%s", placetype),
-		fmt.Sprintf("wof:id=%d", *wofid),
-		fmt.Sprintf("archive:dt=%s", dt),
-	}
-
-	for _, t := range concordances {
-		tags = append(tags, t)
-	}
-
-	for _, t := range wof_tags {
-		tags = append(tags, t)
-	}
-
-	for id, pt := range hierarchy {
-		t := fmt.Sprintf("wof:%s=%s", pt, id)
-		tags = append(tags, t)
-	}
-
-	bm := Bookmark{
-		URL:   *to_archive,
-		Title: title,
-		Tags:  tags,
-	}
-
-	enc_bm, err := json.Marshal(bm)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("%s\n", pretty.Pretty(enc_bm))
-
-	// go!
-
-	err = SaveBookmark(bm, *token)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func GetBookmark(uri string, token string) error {
-
-	req, err := http.NewRequest("GET", "https://api.pinboard.in/v1/posts/get", nil)
-
-	if err != nil {
-		return err
-	}
-
-	params := url.Values{}
-
-	params.Set("url", uri)
-	params.Set("auth_token", token)
-	params.Set("format", "json")
-
-	req.URL.RawQuery = params.Encode()
-
-	client := &http.Client{}
-
-	rsp, err := client.Do(req)
-
-	if err != nil {
-		return err
-	}
-
-	defer rsp.Body.Close()
-
-	if rsp.StatusCode != 200 {
-		return errors.New(rsp.Status)
-	}
-
-	body, err := ioutil.ReadAll(rsp.Body)
-
-	if err != nil {
-		return err
-	}
-
-	/*
-
-	2017/05/30 20:07:11 {"date":"2017-05-31T01:10:23Z","user":"mapzen","posts":[{"href":"https:\/\/missionlocal.org\/2016\/10\/trump-supporters-kicked-out-of-zeitgeist-bar-for-lewd-comments\/","description":"Trump Supporters Kicked Out of Zeitgeist Bar for Lewd Comments \u00bb MissionLocal","extended":"","meta":"725e08849afb1c22b5db857a711eda19","hash":"ab1c141c11e9d3522ff03c53ae3cf996","time":"2017-05-31T01:10:23Z","shared":"yes","toread":"no","tags":"missionlocal.org wof:placetype=venue wof:id=588371677 archive:dt=20170531011022 wof:region_id=85688637 wof:venue_id=588371677 wof:continent_id=102191575 wof:country_id=85633793 wof:county_id=102087579 wof:locality_id=85922583 wof:neighbourhood_id=85887415"}]}
-
-	*/
-	
-	log.Println(string(body))
-
-	return nil
-
-}
-
-func SaveBookmark(bm Bookmark, token string) error {
-
-	req, err := http.NewRequest("GET", "https://api.pinboard.in/v1/posts/add", nil)
-
-	if err != nil {
-		return err
-	}
-
-	params := bm.ToParams()
-	params.Set("auth_token", token)
-	req.URL.RawQuery = (*params).Encode()
-
-	client := &http.Client{}
-
-	rsp, err := client.Do(req)
-
-	if err != nil {
-		return err
-	}
-
-	defer rsp.Body.Close()
-
-	if rsp.StatusCode != 200 {
-		return errors.New(rsp.Status)
-	}
-
-	return nil
-}
-
-func ArchiveURL(u string) (string, error) {
+func (wb *WaybackAPI) SaveURL(u string) (string, error) {
 
 	/*
 
@@ -312,7 +62,328 @@ func ArchiveURL(u string) (string, error) {
 	return dt, nil
 }
 
-func ParseURL(url string) (string, error) {
+type PinboardAPI struct {
+	AuthToken string
+}
+
+func NewPinboardAPI(auth_token string) (*PinboardAPI, error) {
+
+	p := PinboardAPI{
+		AuthToken: auth_token,
+	}
+
+	return &p, nil
+}
+
+func (p *PinboardAPI) SaveBookmark(bm *Bookmark) error {
+
+	req, err := http.NewRequest("GET", "https://api.pinboard.in/v1/posts/add", nil)
+
+	if err != nil {
+		return err
+	}
+
+	params := bm.ToParams()
+	params.Set("auth_token", p.AuthToken)
+	req.URL.RawQuery = (*params).Encode()
+
+	client := &http.Client{}
+
+	rsp, err := client.Do(req)
+
+	if err != nil {
+		return err
+	}
+
+	defer rsp.Body.Close()
+
+	if rsp.StatusCode != 200 {
+		return errors.New(rsp.Status)
+	}
+
+	return nil
+}
+
+func (p *PinboardAPI) GetBookmark(uri string) (*Bookmark, error) {
+
+	req, err := http.NewRequest("GET", "https://api.pinboard.in/v1/posts/get", nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	params := url.Values{}
+
+	params.Set("url", uri)
+	params.Set("auth_token", p.AuthToken)
+	params.Set("format", "json")
+
+	req.URL.RawQuery = params.Encode()
+
+	client := &http.Client{}
+
+	rsp, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rsp.Body.Close()
+
+	if rsp.StatusCode != 200 {
+		return nil, errors.New(rsp.Status)
+	}
+
+	body, err := ioutil.ReadAll(rsp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	post := gjson.GetBytes(body, "posts.0")
+
+	if !post.Exists() {
+		return nil, nil
+	}
+
+	var url string
+	var title string
+	var tags []string
+
+	for k, v := range post.Map() {
+
+		switch k {
+		case "href":
+			url = v.String()
+		case "description":
+			title = v.String()
+		case "tags":
+			tags = strings.Split(v.String(), " ")
+		default:
+			// pass
+		}
+	}
+
+	bm := Bookmark{
+		URL:   url,
+		Title: title,
+		Tags:  tags,
+	}
+
+	return &bm, nil
+
+}
+
+type Bookmark struct {
+	URL   string
+	Title string
+	Tags  []string
+}
+
+func (bm Bookmark) ToParams() *url.Values {
+
+	params := url.Values{}
+
+	params.Set("url", bm.URL)
+	params.Set("description", bm.Title)
+	params.Set("tags", strings.Join(bm.Tags, " "))
+
+	return &params
+}
+
+func main() {
+
+	var wofid = flag.Int64("wofid", 0, "A valid Who's On First ID")
+	var to_archive = flag.String("url", "", "The URL you want to bookmark.")
+	var auth_token = flag.String("auth-token", "", "A valid Pinboard API auth token.")
+	var data_root = flag.String("data-root", "https://whosonfirst.mapzen.com/data/", "...")
+
+	flag.Parse()
+
+	if *wofid == 0 {
+		log.Fatal("Missing WOF ID")
+	}
+
+	if *to_archive == "" {
+		log.Fatal("Missing URL")
+	}
+
+	if *auth_token == "" {
+		log.Fatal("Missing Pinboard API token")
+	}
+
+	pb, err := NewPinboardAPI(*auth_token)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	wb, err := NewWaybackAPI()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Who are we trying to bookmark
+
+	parsed, err := url.Parse(*to_archive)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	host := parsed.Host
+
+	if strings.HasPrefix(host, "www.") {
+		host = strings.Replace(host, "www.", "", -1)
+	}
+
+	// Check to see if we've already bookmarked this
+
+	old_bm, err := pb.GetBookmark(*to_archive)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// fetch the WOF record and extract hierarchy and concordances
+	// please move me in to a function or something...
+	// (20170530/thisisaaronland)
+
+	hierarchy := make(map[string]string)
+	concordances := make([]string, 0)
+	// placetypes
+	
+	abs_url, err := uri.Id2AbsPath(*data_root, *wofid)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rsp, err := http.Get(abs_url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rsp.Body.Close()
+
+	feature, err := ioutil.ReadAll(rsp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	h := gjson.GetBytes(feature, "properties.wof:hierarchy")
+
+	h.ForEach(func(key, value gjson.Result) bool {
+
+		for placetype, id := range value.Map() {
+			hierarchy[id.String()] = placetype
+		}
+
+		return true // keep iterating
+	})
+
+	p := gjson.GetBytes(feature, "properties.wof:placetype")
+	placetype := p.String()
+
+	c := gjson.GetBytes(feature, "properties.wof:concordances")
+
+	if c.Exists() {
+
+		c.ForEach(func(key, value gjson.Result) bool {
+
+			if key.String() != "sg:id" {
+				tag := fmt.Sprintf("%s=%s", key, value)
+				concordances = append(concordances, tag)
+			}
+
+			return true
+		})
+	}
+
+	wof_tags := make([]string, 0)
+
+	t := gjson.GetBytes(feature, "properties.wof:tags")
+
+	if t.Exists() {
+
+		t.ForEach(func(key, value gjson.Result) bool {
+			wof_tags = append(wof_tags, value.String())
+			return true
+		})
+	}
+
+	// get URL title
+
+	title, err := GetTitle(*to_archive)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// archive URL and get datetime stamp
+
+	dt, err := wb.SaveURL(*to_archive)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// build bookmark
+
+	tags := []string{
+		host,
+		fmt.Sprintf("wof:placetype=%s", placetype),
+		fmt.Sprintf("wof:id=%d", *wofid),
+		fmt.Sprintf("archive:dt=%s", dt),
+	}
+
+	for _, t := range concordances {
+		tags = append(tags, t)
+	}
+
+	for _, t := range wof_tags {
+		tags = append(tags, t)
+	}
+
+	for id, pt := range hierarchy {
+		t := fmt.Sprintf("wof:%s=%s", pt, id)
+		tags = append(tags, t)
+	}
+
+	if old_bm != nil {
+
+		for _, t := range old_bm.Tags {
+			tags = append(tags, t)
+		}
+	}
+
+	new_bm := Bookmark{
+		URL:   *to_archive,
+		Title: title,
+		Tags:  tags,
+	}
+
+	enc_bm, err := json.Marshal(new_bm)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s\n", pretty.Pretty(enc_bm))
+
+	// go!
+
+	err = pb.SaveBookmark(&new_bm)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func GetTitle(url string) (string, error) {
 
 	rsp, err := http.Get(url)
 
